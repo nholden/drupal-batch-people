44141d215847e4170c78e5eac7516e934c47f641
Nick Holden <nholden@bu.edu>
2015-04-10 13:38:39 -0400

    Add login method

--- create_people.rb
+++ create_people.rb
@@ -1,6 +1,21 @@
+require 'capybara'
 require 'yaml'
 CONFIG = YAML.load_file("config.yml")
 
-require 'capybara'
-session = Capybara::Session.new(:selenium)
-session.visit CONFIG['drupal_url']
+class AddPeople
+  include Capybara::DSL
+
+  def initialize
+    Capybara.default_driver = :selenium
+  end
+
+  def login
+    visit "#{CONFIG['drupal_url']}/user"
+    fill_in('Username', :with => CONFIG['username'])
+    fill_in('Password', :with => CONFIG['password'])
+    click_button('Log in')
+    false if !has_content?("People")
+  end
+end
+
+AddPeople.new.login
